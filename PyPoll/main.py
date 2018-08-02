6a9e55c0a8e6238bd9bf63ba9354212146ec6354
Troy-Bailey <troy@troybailey.com>
2018-08-02 16:19:13 -0500

    python homework with second problem working need to add output

---
+++ PyPoll/main.py
@@ -0,0 +1,50 @@
+# Install Modules
+import os
+import csv
+from pathlib import Path
+
+# Setup path to read csv file
+csvpath = Path("election_data.csv")
+
+# Set up variables
+voteTally = {}
+voteCount = 0
+winnerName = ""
+winnerCount = 0
+
+# Open csv file
+with open (csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+# Read the header row
+    csv_header = next(csvreader)
+
+# Loop through file
+    for row in csvreader:
+        voteCount = voteCount + 1
+
+# If the candidate is in the dictionary, add 1 to their tally
+        if (row[2] in voteTally):
+            voteTally[row[2]] = voteTally[row[2]]  + 1
+
+# If not, initialize the candidate with a tally of 1
+        else:
+            voteTally[row[2]] = 1
+
+
+# Set up analysis headers
+print("Election Results")
+print("-----------------------")
+print("Total Votes: " + str(voteCount))
+print("-----------------------")
+# Loop through the tally and create the datatable
+for candidate in voteTally:
+    if voteTally[candidate] > winnerCount:
+        winner = candidate
+        winnerCount = voteTally[candidate]
+    print(candidate, str(round(voteTally[candidate]/voteCount*100,4)) + "% (" + str(voteTally[candidate]) + ")")
+print("-----------------------")
+print("Winner: " + winner)
+print("-----------------------")
+
+
